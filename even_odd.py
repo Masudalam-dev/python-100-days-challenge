3ec5a9cdf0acf1305195e06bfe520bef5329af90
Masud Alam <masud.alam.it2003@gmail.com>
2026-03-26 18:21:09 +0530

    To Know Either The Number is Even or Odd.

---
+++ even_odd.py
@@ -0,0 +1,6 @@
+# To find out whether the number is Even or Odd using if else and modulo
+num = int(input("Enter any number to know Even or Odd: "))
+if num % 2 == 0:
+    print(num,"Is a Even Number.")
+else:
+    print(num,"Is a Odd Number.")
