04c2aebf63bb64479e1405d387f9cf9a94518e24
Masud Alam <masud.alam.it2003@gmail.com>
2026-03-26 19:08:24 +0530

    Added more elif and condition

--- Day-03-Treasure-Island/rollercoaster.py
+++ Day-03-Treasure-Island/rollercoaster.py
@@ -6,8 +6,14 @@ if height >= 110:
     print("You can ride rollercoaster 😎 ")
     # Make sure you write age variable inside conditional statement otherwise it will work as global and also work for first else
     age = int(input("How old are you? "))
+    gender = input("Your Gender: Male  Female  Others(Please write the first letter in Capital). ")
     if age < 12:
-        print("You need to pay $6.")
+        if gender == "Male":
+            print("You need to pay only 5$.")
+        elif gender == "Female":
+            print("You need to pay just $2.")
+        else:
+            print("You are Welcome!")
     elif age <= 18:
         print("You need to pay $8.")
     else:
