9551e7764be9107ffb861b7d270b390560ecda9a
Masud Alam <masud.alam.it2003@gmail.com>
2026-03-26 18:50:55 +0530

    I have added that someone can either  ride a rollercoaster or not

---
+++ Day-03-Treasure-Island/rollercoaster.py
@@ -0,0 +1,15 @@
+# Nested if else
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm ? "))
+
+if height >= 110:
+    print("You can ride rollercoaster 😎 ")
+    # Make sure you write age variable inside conditional statement otherwise it will work as global and also work for first else
+    age = int(input("How old are you? "))
+    if age <= 18:
+        print("You need to pay $6.")
+    else:
+        print("You need to pay $10.")
+
+else:
+    print("First Grow up BRO 😍")
