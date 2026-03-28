635d36ef50acab9ed9fdf2341807399d365e3db9
Masud Alam <masud.alam.it2003@gmail.com>
2026-03-28 19:09:13 +0530

    Who will pay the restaurant bill

---
+++ Project-04-Rock-Paper-Scissors/who_pay_bill.py
@@ -0,0 +1,5 @@
+import random
+print("Welcome to the Game of paying Bill in the Restaurant.")
+friends = ["Masud","Sajid","Shoaib","Roshan","Ismail","Ankit"]
+random_name = random.choice(friends)
+print(f"{random_name} will pay the bill.")
