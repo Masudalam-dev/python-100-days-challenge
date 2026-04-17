f938f53ef88ae12ca874df0f0f3fe77fdc4e515b
Masud Alam <masud.alam.it2003@gmail.com>
2026-04-18 00:33:30 +0530

    added encrypt and decrypt functions of caesar project

--- Project-08-Caesar-Cipher/main.py
+++ Project-08-Caesar-Cipher/main.py
@@ -1,14 +1,40 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 route = input("Type 'encode' for Encrypt and type 'decode' for Decrypt.\n").lower()
 message = input("Type your message!\n").lower()
-shift_num = int(input("Type the shift number: "))
+shift = int(input("Type the shift number: "))
 
-output = ""
-for char in message:
-    if char in alphabet:
-        shifted_value = (alphabet.index(char) + shift_num) % 26
-        output += alphabet[shifted_value]
-    else:
-        output += char
-print("Your encode result is",output)
+def encrypt(original_text,shift_num):
+    output = ""
+    for char in original_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = (position + shift_num) % 26
+            output += alphabet[new_position]
+        else:
+            output += char
+    print(f"Your encode result: {output}")
+
+
+
+def decrypt(original_text,shift_num):
+    output = ""
+    for char in original_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = (position - shift_num) % 26
+            output += alphabet[new_position]
+    print(f"Your decode result: {output}")
+
+
+if route == "encode":
+    encrypt(original_text=message, shift_num=shift)
+
+elif route == "decode":
+    decrypt(original_text=message, shift_num=shift)
+else:
+    print("Invalid input!")
+
+
+
 
