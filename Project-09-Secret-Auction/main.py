1e88fc459094b5b1f42be26d4473ba0edc0e44ed
Masud Alam <masud.alam.it2003@gmail.com>
2026-04-24 22:06:47 +0530

    Completed project 09

--- Project-09-Secret-Auction/main.py
+++ Project-09-Secret-Auction/main.py
@@ -1,33 +1,48 @@
 # Store the name of auction participant and bid amount in list inside dictionary
-auction = {
-    "names": [],
-    "bid_values": []
-}
+
+auction = {}
 
 # Ask the participant of their name and bid amount.
 # .strip() compromise the mistaken spaces and .title() convert the first letter to capital.
 # I will use while loop here because I don't know the number of participant
-condition = True
-while condition:
-    while condition:
-        participant_name = input("Enter your name: ").strip().title()
-        if not participant_name:
+
+while True:
+
+    # Name Input
+    while True:
+        participant = input("Enter your name: ").strip().title()
+        if not participant:
             print("For identification you need to enter your name!")
-        elif participant_name in auction:
+        elif participant in auction:
             print("You are already in the list.")
         else:
             break
 
-    while condition:
-        bid_amount = input("Enter your bid in $: ").isdigit()
-        if bid_amount:
-            print("Added")
-            break
-        else:
+    # Bid Input
+    while True:
+        bid_amount = input("Enter your bid in $: ")
+        if not bid_amount.isdigit():
             print("Enter amount in numbers.")
+        else:
+            bid_amount = int(bid_amount)
+            break
 
-    more_bid = input("You want add more bidder 'yes' or 'no'")
+    # Store the data into dictionary
+    auction[participant] = bid_amount
+    print("Bid added successfully!\n")
+
+    # Asking user to add more bidder or exit
+    more_bid = input("You wanna add more bidder 'yes' or 'no'")
     if more_bid.lower() != "yes":
         break
 
 
+# to get the winner
+highest_bid = 0
+winner = ""
+
+for name in auction:
+    if auction[name] > highest_bid:
+        highest_bid = auction[name]
+        winner = name
+print(f"The winner is {winner} with ${highest_bid} bid.")
